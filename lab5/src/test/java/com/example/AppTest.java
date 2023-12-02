02f115f0332cf47a5c481ba67e3f3474e39e2780
Artandrey <artandrey7777@gmail.com>
2023-12-02 23:09:58 +0200

    added test

--- lab5/src/test/java/com/example/AppTest.java
+++ lab5/src/test/java/com/example/AppTest.java
@@ -1,9 +1,11 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Before;
 import org.junit.Test;
+import com.example.exceptions.AccountNotFoundException;
+import com.example.exceptions.NegativeAmountException;
 
 /**
  * Unit test for simple App.
@@ -18,7 +20,18 @@ public class AppTest {
     }
 
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void shouldThrowExceptionIfAccountNotFound() {
+        assertThrows(AccountNotFoundException.class, () -> {
+            bank.findAccount(0);
+        });
     }
+
+    @Test
+    public void shouldThrowInsufficientAmountEception() {
+        assertThrows(NegativeAmountException.class, () -> {
+            bank.createAccount("Name 1", -10);
+        });
+    }
+
+    // @Test void
 }
