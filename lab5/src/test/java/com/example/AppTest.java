bfc3ed198f13d2c1c328a419125e2d7734ee39ce
Artandrey <artandrey7777@gmail.com>
2023-12-03 00:16:24 +0200

    completed test

--- lab5/src/test/java/com/example/AppTest.java
+++ lab5/src/test/java/com/example/AppTest.java
@@ -5,11 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.Before;
 import org.junit.Test;
 import com.example.exceptions.AccountNotFoundException;
+import com.example.exceptions.InsufficientFundsException;
 import com.example.exceptions.NegativeAmountException;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
     private Bank bank;
@@ -33,5 +31,13 @@ public class AppTest {
         });
     }
 
-    // @Test void
+    @Test
+    void shouldThrowInsufficientFundsException() {
+        long account1 = bank.createAccount("Name 1", 10);
+        long account2 = bank.createAccount("Name 2", 10);
+
+        assertThrows(InsufficientFundsException.class, () -> {
+            bank.transferMoney(account1, account2, 30);
+        });
+    }
 }
